732a031a5480ce126aa8124df394b7dd1bd94f8d
chauvanhieu <chauhieu.nina@gmail.com>
2022-11-05 12:13:49 +0700

    asd

---
+++ src/NewMain.java
@@ -0,0 +1,22 @@
+
+import java.util.Date;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(HELPER.helper.LayNgayString(new Date(), "yyyy-MM-dd"));
+    }
+    
+}
